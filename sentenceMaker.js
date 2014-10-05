5c46e1bd2f314c7bbc630f8924566c65c764f04d
nolug <ngolub@stanford.edu>
2014-10-04 18:17:57 -0700

    Update sentenceMaker.js

--- sentenceMaker.js
+++ sentenceMaker.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
 var start1 = 'How dare you <verb> me you <adjective> <adjective> <noun>';
-var start2 = 'If you ever <verb> me again you <noun> you will be even more of <noun>'
+var start2 = 'If you ever <verb> me again you <adjective> <noun> <noun> I will call the police'
 var start3 = "Why don't you just <command> you <adjective> <adjective> <noun>"
 var start4 = "How come <noun>s like you think you can just <verb> and <verb> people like me"
 var start5 = "Please <command> you <adjective> <noun> and then <command>"
 var start6 = "Oh just <command> you <adjective> <noun> before you <verb> any more innocent children"
 var start7 = "I'm tired of <adjective> <adjective> <noun>s like you who constantly <verb> and <verb> women like me. Do us all a favor and <command>"
-var start8 = "Please "
+var start8 = "You think you are so cool with your <adjective> <adjective> <adjective> <noun> rhetoric. You aren't."
+var start9 = "Every time you <verb> an orphan you make the world a more <adjective> and <adjective> place"
 var verb = ['sexify','sexualize','insult','eye-rape', 'misogynize', 'accost', 'patronize','hypersexualize',
 'spit heternormative libel in front of', 'interrupt', 'label', 'genderfy', 'stereotypize', 'racify', 'supersexualize','oppress', 'sexile', 'offend', 'objectify'];
 var adjective = ['heteronormative', 'misogynistic', 'neckbeard','enabling', 'arrogant', 'homophobic', 'heterophobic',
@@ -15,10 +16,11 @@ var adjective = ['heteronormative', 'misogynistic', 'neckbeard','enabling', 'arr
 var noun = ['racist', 'misogynist', 'baby-slapper', 'seal-clubber', 'republican', 'woman-hater', 'deviant','predator',
 'fascist', 'villian', 'creeper', 'stalker', 'cretin', 'vagina-puncher', 'boob-hater', 'man-whore', 'piece of shit'];
 var command = ["go away", "die", "eat slugs", "play in traffic", 'leave me be', 'eat arsenic', 'attend a feminist conference',
-'apologize', 'take a walk', 'go to a cultural sensitivity training class', 'eat shit', 'watch your privelige', 'drink acid', 'fly a kite in a rainstorm']
+'apologize', 'take a walk', 'go to a cultural sensitivity training class', 'eat shit', 'watch your privelige', 'drink acid', 'fly a kite in a rainstorm',
+'swim in a puddle of lava', 'jump off of a building']
 
 var map = {'<start1>': start1, '<verb>':verb, '<adjective>':adjective,'<noun>' : noun, '<command>': command};
-var initialMap = [start1, start2, start3, start4,start5,start6,start7];
+var initialMap = [start1, start2, start3, start4,start5,start6,start7,start8,start9];
 
 
 function stringParser(s) {
@@ -45,9 +47,9 @@ function randomSentence(){
 }
 
 $("button").click(function(){
-
-
-		$("p").text('"' + stringParser(randomSentence()) + '"');
+		$('p').fadeOut('medium', function() {$("p").text('"' + stringParser(randomSentence()) + '"');});
+		$('p').fadeIn('medium');
+		//$("p").text('"' + stringParser(randomSentence()) + '"');
 	});
 
 
